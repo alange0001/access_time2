3f69ad94ace226a0c64ff1c9f04666ff11bad1b2
Adriano Lange <alange0001@gmail.com>
2020-03-30 20:25:59 -0400

    new parameters and metadata manipulation

--- access_time2.go
+++ access_time2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -11,84 +12,90 @@ import (
 )
 
 type Options struct {
-	directory         string
-	fileSize          uint
-	filesystemPercent uint
-	numberOfFiles     uint
-	blockSize         uint
-	writeRatio        float64
-	writeRatioThread0 float64
-	time              uint
+	Directory         string
+	FileSize          uint
+	FilesystemPercent uint
+	NumberOfFiles     uint
+	BlockSize         uint
+	WriteRatio        float64
+	WriteRatioThread0 float64
+	Time              uint
+	Runs              uint
 }
 
-func parseArgs() *Options {
+func parseArgs() (*Options, *syscall.Statfs_t) {
 	var options Options
-	flag.StringVar(&options.directory, "directory", "", "working directory")
-	flag.UintVar(&options.fileSize, "file-size", 100, "file size (MB)")
-	flag.UintVar(&options.filesystemPercent, "filesystem-percent", 0, "percent of the filesystem used in the experiment (override file-size)")
-	flag.UintVar(&options.numberOfFiles, "number-of-files", 1, "number of files to read/write")
-	flag.UintVar(&options.blockSize, "block-size", 4, "block size (KB)")
-	flag.Float64Var(&options.writeRatio, "write-ratio", -1, "write ratio (writes/reads)")
-	flag.Float64Var(&options.writeRatioThread0, "write-ratio-thread0", -1, "write ratio of thread0 (writes/reads)")
-	flag.UintVar(&options.time, "time", 7, "time")
+	flag.StringVar(&options.Directory, "directory", "", "working directory")
+	flag.UintVar(&options.FileSize, "file-size", 100, "file size (MB)")
+	flag.UintVar(&options.FilesystemPercent, "filesystem-percent", 0, "percent of the filesystem used in the experiment (override file-size)")
+	flag.UintVar(&options.NumberOfFiles, "number-of-files", 1, "number of files to read/write")
+	flag.UintVar(&options.BlockSize, "block-size", 4, "block size (KB)")
+	flag.Float64Var(&options.WriteRatio, "write-ratio", -1, "write ratio (writes/reads)")
+	flag.Float64Var(&options.WriteRatioThread0, "write-ratio-thread0", -1, "write ratio of thread0 (writes/reads)")
+	flag.UintVar(&options.Time, "time", 7, "experiment time per run")
+	flag.UintVar(&options.Runs, "runs", 1, "number of runs per write-ratio")
 	flag.Parse()
 
-	log.Println("Options:")
-	log.Printf("   directory=%v, file-size=%v, filesystem-percent=%v, "+
-		"number-of-files=%v, block-size=%v",
-		options.directory, options.fileSize, options.filesystemPercent,
-		options.numberOfFiles, options.blockSize)
-	log.Printf("   write-ratio=%v, write-ratio-thread0=%v, time=%v",
-		options.writeRatio, options.writeRatioThread0, options.time)
+	j, _ := json.Marshal(options)
+	log.Printf("Options Received: %v", string(j))
 
-	aux, err := os.Stat(options.directory)
+	aux, err := os.Stat(options.Directory)
 	if err != nil || !aux.IsDir() {
 		log.Fatal("invalid directory")
 	}
-	if options.fileSize < 10 {
+	if options.FileSize < 10 {
 		log.Fatal("file-size must be >= 10")
 	}
-	if options.filesystemPercent > 100 {
+	if options.FilesystemPercent > 100 {
 		log.Fatal("filesystem-percent must be <= 100")
 	}
-	if options.blockSize < 4 || options.blockSize > (options.fileSize*1024) {
+	if options.BlockSize < 4 || options.BlockSize > (options.FileSize*1024) {
 		log.Fatal("invalid block-size")
 	}
-	if options.numberOfFiles < 1 {
+	if options.NumberOfFiles < 1 {
 		log.Fatal("number-of-files must be > 0")
 	}
-	if options.writeRatio > 1 {
+	if options.WriteRatio > 1 {
 		log.Fatal("write-ratio must be between 0 and 1")
 	}
-	if options.writeRatioThread0 > 1 {
+	if options.WriteRatioThread0 > 1 {
 		log.Fatal("write-ratio-thread0 must be between 0 and 1")
 	}
-	if options.time < 5 {
-		log.Fatal("time must be >= 5")
+	if options.Time < 1 {
+		log.Fatal("time must be >= 1")
+	}
+	if options.Runs < 1 {
+		log.Fatal("runs must be >= 1")
+		if options.WriteRatio < 0 {
+			log.Fatal("--write-ratio must be defined with the parameter --runs")
+		}
 	}
 
-	return &options
-}
-
-func main() {
-	options := parseArgs()
-	statfs := getFilesystemStats(options.directory)
-	ok := make(chan int)
-	threads := make([]*Thread, options.numberOfFiles)
-
-	if options.filesystemPercent > 0 {
+	statfs := getFilesystemStats(options.Directory)
+	if options.FilesystemPercent > 0 {
 		percentBlocks := (uint(statfs.Bavail) * uint(statfs.Bsize)) / uint(1024*1024) // in MiB
-		percentBlocks = (percentBlocks * options.filesystemPercent) / uint(100)       // percentage
-		options.fileSize = percentBlocks / uint(options.numberOfFiles)                // per file/thread
-		log.Printf("filesystem-percent defined to %v%%. Overriding file-size to %v", options.filesystemPercent, options.fileSize)
+		percentBlocks = (percentBlocks * options.FilesystemPercent) / uint(100)       // percentage
+		options.FileSize = percentBlocks / uint(options.NumberOfFiles)                // per file/thread
+		log.Printf("filesystem-percent defined to %v%%. Overriding file-size to %v", options.FilesystemPercent, options.FileSize)
 	}
-	if ((options.blockSize * 1024) % uint(statfs.Bsize)) != 0 {
+	if ((options.BlockSize * 1024) % uint(statfs.Bsize)) != 0 {
 		log.Fatal(fmt.Sprintf("block-size must be multiple of %v KiB", statfs.Bsize/1024))
 	}
 
+	j, _ = json.Marshal(options)
+	log.Printf("Options Processed: %v", string(j))
+
+	return &options, statfs
+}
+
+func main() {
+	options, statfs := parseArgs()
+	ok := make(chan int)
+	threads := make([]*Thread, options.NumberOfFiles)
+
 	////// Creating data for the Threads and creating files //////
-	for i := uint(0); i < options.numberOfFiles; i++ {
-		t := NewThread(i, fmt.Sprintf("%v/%v", options.directory, i), options, statfs)
+	for i := uint(0); i < options.NumberOfFiles; i++ {
+		t := NewThread(i, fmt.Sprintf("%v/%v", options.Directory, i), options, statfs)
 		threads[i] = t
 		go t.createFile(ok)
 	}
@@ -98,35 +105,48 @@ func main() {
 
 	////// Defining writeRatios //////
 	var writeRatios []float64
-	if options.writeRatio < 0 {
+	if options.WriteRatio < 0 {
 		writeRatios = make([]float64, 11)
 		for i := 0; i < 11; i++ {
 			writeRatios[i] = float64(i) / float64(10)
 		}
 	} else {
 		writeRatios = make([]float64, 1)
-		writeRatios[0] = options.writeRatio
+		writeRatios[0] = options.WriteRatio
 	}
 
 	////// Experiment Loop //////
+	timeStart := time.Now()
 	var r float64
 	for _, ratio := range writeRatios {
-		for i, t := range threads {
-			if i == 0 && options.writeRatioThread0 >= 0 {
-				r = options.writeRatioThread0
-			} else {
-				r = ratio
+		for runs := uint(0); runs < options.Runs; runs++ {
+			for i, t := range threads {
+				if i == 0 && options.WriteRatioThread0 >= 0 {
+					r = options.WriteRatioThread0
+				} else {
+					r = ratio
+				}
+				go t.processFile(r, ok)
 			}
-			go t.processFile(r, ok)
+			for range threads {
+				<-ok
+			}
+			ratioThread0 := ratio
+			if options.WriteRatioThread0 >= 0 {
+				ratioThread0 = options.WriteRatioThread0
+			}
+			// Print Results: //
+			fmt.Printf("%.2f, %.1f, %.1f", time.Since(timeStart).Seconds(), ratioThread0, ratio)
+			var sum uint64
+			for _, t := range threads {
+				sum += t.throughput
+			}
+			fmt.Printf(", %v", sum)
+			for _, t := range threads {
+				fmt.Printf(", %v", t.throughput)
+			}
+			fmt.Printf("\n")
 		}
-		for range threads {
-			<-ok
-		}
-		var sum uint64
-		for _, t := range threads {
-			sum += t.throughput
-		}
-		fmt.Printf("%.2f, %v, %v\n", ratio, threads[0].throughput, sum)
 	}
 
 	////// Removing Files //////
@@ -172,10 +192,13 @@ func (thread *Thread) createFile(ok chan int) {
 	}
 	buffer := make([]byte, 1024*1024)
 	rand.Read(buffer)
-	for i := uint(0); i < thread.options.fileSize; i++ {
+	for i := uint(0); i < thread.options.FileSize; i++ {
 		if _, err := syscall.Write(fdLocal, buffer); err != nil {
 			log.Fatal(fmt.Sprintf("thread %v: Error writing file %v: %v", thread.id, thread.filename, err))
 		}
+	}
+	if err := syscall.Fsync(fdLocal); err != nil {
+		log.Fatal(fmt.Sprintf("thread %v: Fsync error: %v", thread.id, err))
 	}
 	thread.fd = fdLocal
 	ok <- 1
@@ -193,14 +216,14 @@ func (thread *Thread) removeFile() {
 }
 
 func (thread *Thread) processFile(writeRatio float64, ok chan int) {
-	fileBlocks := int64((thread.options.fileSize * 1024) / thread.options.blockSize)
-	buffer := make([]byte, thread.options.blockSize*1024)
+	fileBlocks := int64((thread.options.FileSize * 1024) / thread.options.BlockSize)
+	buffer := make([]byte, thread.options.BlockSize*1024)
 	rand.Read(buffer)
 
-	log.Printf(fmt.Sprintf("thread %v: main loop, writeRatio=%.2f", thread.id, writeRatio))
+	log.Printf(fmt.Sprintf("thread %v: main loop, writeRatio=%.1f", thread.id, writeRatio))
 	var count uint64
 	timeStart := time.Now()
-	for uint(time.Since(timeStart).Seconds()) < thread.options.time {
+	for uint(time.Since(timeStart).Seconds()) < thread.options.Time {
 		for i := 0; i < 100; i++ {
 			r := rand.Int63n(fileBlocks)
 			syscall.Seek(thread.fd, r*1024, 0)
@@ -217,7 +240,7 @@ func (thread *Thread) processFile(writeRatio float64, ok chan int) {
 		}
 	}
 
-	thread.throughput = uint64((float64(count*uint64(thread.options.blockSize)) / float64(1024)) / time.Since(timeStart).Seconds())
+	thread.throughput = uint64((float64(count*uint64(thread.options.BlockSize)) / float64(1024)) / time.Since(timeStart).Seconds())
 	log.Printf("thread %v: count=%v, time=%.1f, throughput=%v", thread.id, count, time.Since(timeStart).Seconds(), thread.throughput)
 	ok <- 1
 }
--- results/plot.py
+++ results/plot.py
@@ -11,6 +11,7 @@ import collections
 import re
 import sqlite3
 import os
+import json
 import matplotlib.pyplot as plt
 
 class Graphs:
@@ -23,60 +24,71 @@ class Graphs:
 		self.files = []
 		self.conn = sqlite3.connect(':memory:')
 		cur = self.conn.cursor()
-		cur.execute('CREATE TABLE files (id INTEGER PRIMARY KEY, name TEXT, block_size INT, number_of_files INT, filesystem_percent INT, file_size INT, write_ratio_thread0 NUMERIC(3,1))')
-
+		cur.execute('CREATE TABLE files ('+
+			  'id INTEGER PRIMARY KEY, FileName TEXT, BlockSize INT, '+
+			  'NumberOfFiles INT, FilesystemPercent INT, FileSize INT, '+
+			  'Runs INT, WriteRatioThread0 NUMERIC(3,1))')
 		idx = 0
 		for i in os.listdir():
 			if re.search(r'\.csv$', i) is not None:
 				self.filenames.append(i)
 				f = File(i)
 				self.files.append(f)
-				cur.execute("INSERT INTO files VALUES ({}, '{}', {}, {}, {}, {}, {})".format(
-					idx, i, f.metadata['block-size'], f.metadata['number-of-files'],
-					f.metadata['filesystem-percent'], f.metadata['file-size'],
-					f.metadata['write-ratio-thread0']))
+				cur.execute("INSERT INTO files VALUES ({}, '{}', {BlockSize}, {NumberOfFiles}, {FilesystemPercent}, {FileSize}, {Runs}, {WriteRatioThread0})".format(
+					idx, i, **f.metadata))
 				idx = idx +1
 		self.conn.commit()
 
-	def main(self):
+	def printAll(self):
 		self.printFiles()
 		self.printTotals()
 
 	def printFiles(self):
 		for file in self.files:
-			fig, ax = plt.subplots()
-			fig.set_figheight(5)
-			ax.grid()
-			ax.plot(file.x, file.y_thread0, '-', lw=1, color='blue', label='thread 0')
-			ax.plot(file.x, file.y_total, '-', lw=1, color='orange', label='total')
-			ax.set_xlabel('writes/reads')
-			ax.set_ylabel('MiB/s')
-			ax.legend(loc='best', ncol=2, frameon=True)
-			fig.savefig(file.metadata['filename'].replace('.csv', '.pdf'))
-			plt.show()
+			file.printGraph(True)
 
 	def printTotals(self):
-		for row_percent in self.conn.cursor().execute('SELECT DISTINCT filesystem_percent FROM files ORDER BY filesystem_percent'):
-			#print(row_percent[0])
+		for row_group in self.query(
+			'SELECT DISTINCT BlockSize, FilesystemPercent, WriteRatioThread0 '+
+			'FROM files'
+		):
 			fig1, ax1 = plt.subplots()
 			fig2, ax2 = plt.subplots()
 			fig1.set_figheight(5)
 			fig2.set_figheight(5)
-			for row_file in self.conn.cursor().execute('SELECT id, number_of_files FROM files WHERE filesystem_percent = {} AND write_ratio_thread0 = -1 ORDER BY number_of_files'.format(row_percent[0])):
+			fig1.set_figwidth(7)
+			fig2.set_figwidth(7)
+			for row_file in self.query(
+				'SELECT id, number_of_files FROM files '+
+				'WHERE BlockSize = {} AND FilesystemPercent = {} '.format(row_group[0], row_group[1])+
+				'AND WriteRatioThread0 = {} AND Runs=1'.format(row_group[2])+
+				'ORDER BY number_of_files'
+			):
 				f = self.files[row_file[0]]
 				ax1.plot(f.x, f.y_total, '-', label=str(row_file[1]))
 				ax2.plot(f.x, f.y_thread0, '-', label=str(row_file[1]))
 			ax1.grid()
 			ax2.grid()
-			ax1.set_xlabel('writes/reads')
-			ax2.set_xlabel('writes/reads')
-			ax1.set_ylabel('MiB/s')
-			ax2.set_ylabel('MiB/s')
-			ax1.legend(loc='best', ncol=2, frameon=True)
-			ax2.legend(loc='best', ncol=2, frameon=True)
-			fig1.savefig('perc{}totals.pdf'.format(row_percent[0]))
-			fig2.savefig('perc{}thread0.pdf'.format(row_percent[0]))
+			xlabel = 'writes/reads{}'.format('' if row_group[2] == -1 else ' (other threads)')
+			filename_t0 = '' if row_group[2] == -1 else 'tz{}'.format(row_group[2])
+			title_t0 = '' if row_group[2] == -1 else ', thread0(w/r)={}'.format(row_group[2])
+			ax1.set(title='total: bs={}, fs%={}{}'.format(row_group[0], row_group[1], title_t0),
+			   xlabel=xlabel, ylabel='MiB/s')
+			title_t0 = '' if row_group[2] == -1 else '(w/r={})'.format(row_group[2])
+			ax2.set(title='thread0{}: bs={}, fs%={}'.format(title_t0, row_group[0], row_group[1]),
+			   xlabel=xlabel, ylabel='MiB/s (thread0)')
+			chartBox = ax1.get_position()
+			ax1.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.9, chartBox.height])
+			chartBox = ax2.get_position()
+			ax2.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.9, chartBox.height])
+			ax1.legend(loc='upper center', bbox_to_anchor=(1.1, 0.8), title='threads', ncol=1, frameon=True)
+			ax2.legend(loc='upper center', bbox_to_anchor=(1.1, 0.8), title='threads', ncol=1, frameon=True)
+			fig1.savefig('aggregated-bs{}fsp{}{}-totals.pdf'.format(row_group[0],row_group[1],filename_t0))
+			fig2.savefig('aggregated-bs{}fsp{}{}-thread0.pdf'.format(row_group[0],row_group[1],filename_t0))
 			plt.show()
+
+	def query(self, sql):
+		return self.conn.cursor().execute(sql)
 
 def tryConvert(value, *types):
 	for t in types:
@@ -91,17 +103,19 @@ def tryConvert(value, *types):
 class File:
 	metadata = None
 	data = None
-	x = None
-	y_thread0 = None
-	y_total = None
+	data_time = None
+	data_writeratio = None
+	data_thread0 = None
+	data_total = None
 	def __init__(self, filename):
 		self.metadata = collections.OrderedDict()
-		self.metadata['filename'] = filename
+		self.metadata['FileName'] = filename
 
 		self.data = []
-		self.x = []
-		self.y_thread0 = []
-		self.y_total = []
+		self.data_time = []
+		self.data_writeratio = []
+		self.data_thread0 = []
+		self.data_total = []
 		with open(filename,newline='') as file:
 			reader = csv.reader(file, delimiter=',')
 			for row in reader:
@@ -109,24 +123,54 @@ class File:
 				for col in row:
 					aux.append( tryConvert(col.strip(' '), int, float) )
 				self.data.append(aux)
-				self.x.append(aux[0])
-				self.y_thread0.append(aux[1])
-				self.y_total.append(aux[2])
+				self.data_time.append(aux[0])
+				self.data_writeratio.append(aux[2])
+				self.data_total.append(aux[3])
+				self.data_thread0.append(aux[4])
 
 		logfile = filename.replace('.csv', '.log')
 		with open(logfile,newline='') as file:
 			s = ''.join(file.readlines())
-			self.metadata['block-size'] = tryConvert(re.findall(r'block-size=([0-9]+)', s)[0], int, float)
-			self.metadata['number-of-files'] = tryConvert(re.findall(r'number-of-files=([0-9]+)', s)[0], int, float)
-			self.metadata['filesystem-percent'] = tryConvert(re.findall(r'filesystem-percent=([0-9]+)', s)[0], int, float)
-			self.metadata['file-size'] = tryConvert(re.findall(r'file-size=([0-9]+)', s)[0], int, float)
-			aux = re.findall(r'file-size to ([0-9]+)', s)
-			if len(aux) > 0: self.metadata['file-size'] = tryConvert(aux[0], int, float)
-			self.metadata['write-ratio-thread0'] = tryConvert(re.findall(r'write-ratio-thread0=([-.0-9]+)', s)[0], int, float)
+			options_re = re.findall(r'Options Processed: (\{[^}]+\})', s)
+			if len(options_re) > 0:
+				options = json.loads(options_re[0])
+				#print(options)
+				for k, v in options.items():
+					self.metadata[k] = v
 
 		#print('============================================')
 		#for k, v in self.metadata.items():
 		#	print("{}: {}".format(k,v))
 
+	def printGraph(self, save=False):
+		if self.metadata['Runs'] == 1:
+			fig, ax = plt.subplots()
+			fig.set_figheight(5)
+			ax.grid()
+			ax.plot(self.data_writeratio, self.data_total,   '-', lw=1, color='blue', label='total')
+			ax.plot(self.data_writeratio, self.data_thread0, '-', lw=1, color='orange', label='thread0')
+			xlabel = 'writes/reads{}',format('' if self.metadata['WriteRatioThread0'] == -1 else ' (other threads)')
+			title_t0 = '' if self.metadata['WriteRatioThread0'] == -1 else '(w/r={})'.format(self.metadata['WriteRatioThread0'])
+			ax.set(title='thread0{title_t0}: bs={BlockSize}, fs%={FilesystemPercent}, threads={NumberOfFiles}'.format(
+				title_t0=title_t0, **self.metadata
+				), xlabel=xlabel, ylabel='MiB/s')
+			ax.legend(loc='best', ncol=1, frameon=True)
+			if save:
+				fig.savefig(self.metadata['FileName'].replace('.csv', '.pdf'))
+			plt.show()
+		else:
+			fig, ax = plt.subplots()
+			fig.set_figheight(5)
+			ax.grid()
+			ax.plot(self.data_time, self.data_total,   '-', lw=1, color='blue', label='total')
+			ax.plot(self.data_time, self.data_thread0, '-', lw=1, color='orange', label='thread0')
+			title_t0 = '' if self.metadata['WriteRatioThread0'] == -1 else ', thread0 w/r={}'.format(self.metadata['WriteRatioThread0'])
+			ax.set(title='bs={BlockSize}, fs%={FilesystemPercent}, threads={NumberOfFiles}{title_t0}, writes/reads={WriteRatio}'.format(
+				title_t0=title_t0, **self.metadata
+				), xlabel='time(s)', ylabel='MiB/s')
+			ax.legend(loc='best', ncol=1, frameon=True)
+			if save:
+				fig.savefig(self.metadata['FileName'].replace('.csv', '.pdf'))
+			plt.show()
+
 g = Graphs()
-g.main()
